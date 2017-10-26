6e01b62da8976b600275e8519672d3a4ff2e88e8
sppowel <sppowel@mob-win-lappy>
2017-10-25 20:16:33 -0400

    setting this thing up

---
+++ coffee.js
@@ -0,0 +1,10 @@
+
+
+var numberOfCups = prompt("how many cups of coffee yo?");
+
+if (numberOfCups < 3) {
+  console.log("howbout more coffee?");
+}
+else {
+  console.log("I'll pass");
+}
---
+++ firstday.js
@@ -0,0 +1,7 @@
+var test = "poop";
+
+var test2 = 5.0;
+
+var str = "this is me" + test;
+
+console.log(str);
